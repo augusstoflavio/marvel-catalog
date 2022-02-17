6c975e624c31fdae87528e6a3a950ee50b75ba44
Augusto Flávio <augusto.flavio@work.co>
2022-02-16 21:04:09 -0300

    feat: reorganize

---
+++ character/src/main/java/com/marvelcatalog/character/data/mock/CharacterDataSourceMock.kt
@@ -0,0 +1,31 @@
+package com.marvelcatalog.character.data.mock
+
+import com.marvelcatalog.character.core.model.Result
+import com.marvelcatalog.character.data.dataSource.CharacterDataSource
+import com.marvelcatalog.character.data.model.Character
+
+class CharacterDataSourceMock: CharacterDataSource {
+
+    private val characters = listOf(
+        Character(
+            id = 1,
+            name = "Homem de ferro",
+            description = "Homem de ferro descrição",
+            thumbnail = ""
+        ),
+        Character(
+            id = 2,
+            name = "Homem Aranha",
+            description = "Homem Aranha",
+            thumbnail = ""
+        )
+    )
+
+    override suspend fun getCharacters(page: Int): Result<List<Character>> {
+        return Result.Success(characters)
+    }
+
+    override suspend fun getCharacterDetail(characterId: Int): Result<Character> {
+        return Result.Success(characters.first { it.id == characterId })
+    }
+}
