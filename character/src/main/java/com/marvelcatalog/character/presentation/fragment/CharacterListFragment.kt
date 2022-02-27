770f0deacf5e3c4fcafd61e1fe80f7fdf728419e
Augusto Flávio <augusto.flavio@work.co>
2022-02-27 06:59:43 -0300

    feat: add character page

--- character/src/main/java/com/marvelcatalog/character/presentation/fragment/CharacterListFragment.kt
+++ character/src/main/java/com/marvelcatalog/character/presentation/fragment/CharacterListFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.marvelcatalog.character.databinding.FragmentCharacterListBinding
 import com.marvelcatalog.character.presentation.adapter.CharacterAdapter
@@ -84,8 +84,9 @@ class CharacterListFragment: Fragment() {
     }
 
     private fun onClickCharacter(characterView: CharacterView) {
-        //TODO ir para a tela de visualização do personagem
-        Toast.makeText(requireContext(), characterView.name, Toast.LENGTH_SHORT).show()
+        findNavController().navigate(
+            CharacterListFragmentDirections.toCharacterFragment().setCharacterId(characterView.id)
+        )
     }
 
     override fun onDestroyView() {
