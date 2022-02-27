24274f14b9366be2569ba872a5084d57f63186ed
Augusto Flávio <augusto.flavio@work.co>
2022-02-27 07:15:40 -0300

    feat: fill character page

--- character/src/main/java/com/marvelcatalog/character/presentation/fragment/CharacterFragment.kt
+++ character/src/main/java/com/marvelcatalog/character/presentation/fragment/CharacterFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.marvelcatalog.character.databinding.FragmentCharacterBinding
+import com.marvelcatalog.character.presentation.model.CharacterView
+import com.marvelcatalog.commons.extensions.loadImage
 
 class CharacterFragment: Fragment() {
 
     private var _binding: FragmentCharacterBinding? = null
     private val binding get() = _binding!!
+    val args: CharacterFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +23,21 @@ class CharacterFragment: Fragment() {
     ): View {
         _binding = FragmentCharacterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        FakeListCharacter.find(args.characterId)?.let {
+            setupView(it)
+        }
+    }
+
+    private fun setupView(it: CharacterView) {
+        with(binding) {
+            ivThumbnail.loadImage(it.thumbnail)
+            tvTitle.text = it.name
+            tvDescription.text = it.description
+        }
     }
 
     override fun onDestroyView() {
